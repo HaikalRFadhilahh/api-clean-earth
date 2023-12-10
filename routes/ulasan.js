f926cb451c1a0cc3c4ef11c4f4700f0e542a4d38
Haikal R Fadhilah <haikal.fadhilah13@gmail.com>
2023-12-11 00:09:00 +0700

    Prisma: Removing ORM Prisma

--- routes/ulasan.js
+++ routes/ulasan.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require("@prisma/client");
 const checkAuth = require("../middleware/CheckAuth");
-const prisma = new PrismaClient();
 const Validator = require("fastest-validator");
 const pool = require("../database/connection");
 const v = new Validator();
@@ -20,6 +18,7 @@ router.post("/", async (req, res) => {
   }
   const [result, fields] = await connection.execute(query);
   const transformResultData = transformData(result);
+  connection.release();
 
   return res.status(200).json({
     status: "success",
@@ -29,44 +28,48 @@ router.post("/", async (req, res) => {
 });
 
 router.post("/create", checkAuth, async (req, res) => {
+  const connection = await pool.getConnection();
   const schema = {
     bintang: "number|min:1|max:5",
     komentar: "string|empty:false|min:5|max:255",
   };
 
   const validate = v.validate(req.body, schema);
-  const checkUlasan = await prisma.ulasan.findFirst({
-    where: {
-      user_id: req.dataUser.id,
-    },
-  });
+  var [checkUlasan, fields] = await connection.execute(
+    `select * from ulasan where user_id=${req.dataUser.id}`
+  );
+
+  checkUlasan = checkUlasan[0];
 
   if (validate.length) {
+    connection.release();
     return res.status(400).json({
       status: "error",
       message: validate,
     });
   } else if (checkUlasan) {
+    connection.release();
     return res.status(409).json({
       status: "error",
       message: `error insert ulasan,user ${req.dataUser.nama} has submit ulasan`,
     });
   } else {
     try {
-      const insertUlasan = await prisma.ulasan.create({
+      const [result, fields] = await connection.execute(
+        `insert into ulasan(bintang,komentar,user_id) values (${req.body.bintang},'${req.body.komentar}',${req.dataUser.id})`
+      );
+      connection.release();
+      return res.status(200).json({
+        status: "success",
+        message: "success insert data ulasan",
         data: {
           bintang: req.body.bintang,
           komentar: req.body.komentar,
           user_id: req.dataUser.id,
         },
       });
-
-      return res.status(200).json({
-        status: "success",
-        message: "success insert data ulasan",
-        data: insertUlasan,
-      });
     } catch (error) {
+      connection.release();
       return res.status(500).json({
         status: "error",
         message: "server error",
@@ -76,47 +79,47 @@ router.post("/create", checkAuth, async (req, res) => {
 });
 
 router.post("/update", checkAuth, async (req, res) => {
+  const connection = await pool.getConnection();
   const schema = {
     bintang: "number|min:1|max:5",
     komentar: "string|empty:false|min:5|max:255",
   };
 
   const validate = v.validate(req.body, schema);
-  const checkExistUlasan = await prisma.ulasan.findFirst({
-    where: {
-      user_id: req.dataUser.id,
-    },
-  });
+  var [checkExistUlasan, fields] = await connection.execute(
+    `select * from ulasan where user_id=${req.dataUser.id}`
+  );
+  checkExistUlasan = checkExistUlasan[0];
 
   if (validate.length) {
+    connection.release();
     return res.status(400).json({
       status: "error",
       message: validate,
     });
   } else if (checkExistUlasan) {
     try {
-      const updateDataUlasan = await prisma.ulasan.update({
-        where: {
-          user_id: req.dataUser.id,
-        },
+      const [result, fields] = await connection.execute(
+        `update ulasan set bintang=${req.body.bintang},komentar='${req.body.komentar}' where user_id=${req.dataUser.id}`
+      );
+      connection.release();
+      return res.status(200).json({
+        status: "success",
+        message: `succes to edit ulasan for user ${req.dataUser.nama}`,
         data: {
           bintang: req.body.bintang,
           komentar: req.body.komentar,
         },
       });
-
-      return res.status(200).json({
-        status: "success",
-        message: `succes to edit ulasan for user ${req.dataUser.nama}`,
-        data: updateDataUlasan,
-      });
     } catch (error) {
+      connection.release();
       return res.status(500).json({
         status: "error",
         message: "server error",
       });
     }
   } else {
+    connection.release();
     return res.status(404).json({
       status: "error",
       message: `Data Ulasan User ${req.dataUser.nama} Not Found!`,
@@ -131,16 +134,16 @@ function transformData(inputData) {
       bintang: item.bintang,
       komentar: item.komentar,
       user_id: item.user_id,
-      created_at_ulasan: item.ulasan_created_at,
-      updated_at_ulasan: item.ulasan_updated_at,
+      created_at: item.ulasan_created_at,
+      updated_at: item.ulasan_updated_at,
       users: {
         id: item.user_id,
         nama: item.nama,
         username: item.username,
         email: item.email,
         kontak: item.kontak,
-        created_at_user: item.user_created_at,
-        updated_at_user: item.user_updated_at,
+        created_at: item.user_created_at,
+        updated_at: item.user_updated_at,
       },
     };
   });
